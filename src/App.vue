bf8d2e0203461133119116b97158284d1746110c
omar <o.wakeel@intouch.com>
2023-12-19 12:39:32 +0200

    feat: add balance component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
 
 export default defineComponent({
   components: {
     Header,
+    Balance,
   },
 });
 </script>
 
 <template>
   <Header />
+  <div class="container">
+    <Balance />
+  </div>
 </template>
 
 <style scoped></style>
